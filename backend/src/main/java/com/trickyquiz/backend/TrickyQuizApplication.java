3d0b98cd220fd0c24974f9d88a308e5bf552e708
kdtlch3650-ctrl <kdtlch3650@gmail.com>
2026-05-04 18:00:31 +0900

    프로젝트 초기 세팅

---
+++ backend/src/main/java/com/trickyquiz/backend/TrickyQuizApplication.java
@@ -0,0 +1,13 @@
+package com.trickyquiz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrickyQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrickyQuizApplication.class, args);
+	}
+
+}
